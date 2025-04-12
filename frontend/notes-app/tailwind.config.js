eea4a6469446c97d6c41aa5f1bc780a4b369fdfb
ayushagupta <mumbai.ayushgupta@gmail.com>
2025-04-12 01:42:00 -0400

    update login page

--- frontend/notes-app/tailwind.config.js
+++ frontend/notes-app/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Colors used in  project
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E"
+      }
+    },
   },
   plugins: [],
 }
