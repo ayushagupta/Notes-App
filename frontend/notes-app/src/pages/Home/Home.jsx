467aa98d3f7eb60301859a7e7ff6c44389438f20
ayushagupta <mumbai.ayushgupta@gmail.com>
2025-04-14 01:35:45 -0400

    add note card component, arrange in grid

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,12 +1,34 @@
-import React from "react"
-import Navbar from "../../components/Navbar/Navbar"
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
 
 const Home = () => {
-    return (
-        <>
-          <Navbar />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
 
-export default Home
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="My title"
+            date="2 august 1999"
+            content="Hello how are you my friend"
+            tags="#mine"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" onClick={() => {}}>
+        <MdAdd className="text-[32px] text-white" />
+      </button>
+
+    </>
+  );
+};
+
+export default Home;
