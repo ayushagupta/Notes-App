fa0ae1ae712b3adda3663f696e0e9cbf5bdc8efc
ayushagupta <mumbai.ayushgupta@gmail.com>
2025-04-20 23:22:17 -0400

    add api to get all notes

--- backend/index.js
+++ backend/index.js
@@ -191,6 +191,27 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
   }
 });
 
+// Get all notes
+app.get("/get-all-notes", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+
+  try {
+    const notes = await Note.find({ userId: user._id }).sort({ isPinned: -1 });
+
+    return res.json({
+      error: false,
+      notes,
+      message: "All notes retrieved successfully"
+    });
+  } catch (error) {
+    return res.json({
+      error: true,
+      message: "Internal server error"
+    });
+  }
+
+});
+
 app.listen(8000);
 
 module.exports = app;
