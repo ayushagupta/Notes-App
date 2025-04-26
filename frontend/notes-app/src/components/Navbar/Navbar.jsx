c5a0b3589f1d739bafd3e629f6273355f4174e2d
ayushagupta <mumbai.ayushgupta@gmail.com>
2025-04-26 12:04:33 -0400

    integrate user info and logout

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
-const Navbar = () => {
+const Navbar = ({ userInfo }) => {
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const [searchQuery, setSearchQuery] = useState("")
-
-
-  const navigate = useNavigate;
+  const navigate = useNavigate();
 
   const onLogout = () => {
+    localStorage.clear();
     navigate("/login");
   };
 
-
-  const handleSearch = () => {
-
-  }
+  const handleSearch = () => {};
 
   const onClearSearch = () => {
-    setSearchQuery("")
-  }
-
+    setSearchQuery("");
+  };
 
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
@@ -30,14 +25,14 @@ const Navbar = () => {
 
       <SearchBar
         value={searchQuery}
-        onChange={({target}) => {
-          setSearchQuery(target.value)
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
         }}
         handleSearch={handleSearch}
         onClearSearch={onClearSearch}
       />
 
-      <ProfileInfo onLogout={onLogout} />
+      <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
     </div>
   );
 };
